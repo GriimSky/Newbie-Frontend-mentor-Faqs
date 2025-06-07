6dfcd617611e1962f3a533eba1e54889004d8632
Raphael <griimskyow@gmail.com>
2025-06-07 20:31:45 +0300

    faqs frontend mentor yt

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,74 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [openIndex, setOpenIndex] = useState(0);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const questions: { question: string; answer: string }[] = [
+    {
+      question: "What is Frontend Mentor, and how will it help me?",
+      answer:
+        "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with project in HTML, CSS and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+    },
+    {
+      question: "Is Frontend Mentor free?",
+      answer: "Yes, Frontend Mentor is free to use for all developers.",
+    },
+    {
+      question: "Can i use Frontend Mentor projects in my portfolio?",
+      answer:
+        "Yes, Frontend Mentor offers you to use the projects in your portfolio. You can also use it for your personal projects.",
+    },
+    {
+      question: "How can i get help if i'm stuck on challenge?",
+      answer:
+        "You can ask questions in the Frontend Mentor community forum. You can also ask questions in the Frontend Mentor help center.",
+    },
+  ];
+
+  return (
+    <div>
+      <Image
+        src="/background.svg"
+        alt="Logo"
+        width={500}
+        height={500}
+        className="-z-50 absolute top-0 left-0"
+      />
+      <main className="bg-white w-[440px] h-auto rounded-xl ml-4 mt-50 p-5">
+        <div className="ml-5 mr-5">
+          <div className="flex items-center gap-5 mt-5 mb-10">
+            <Image src="/star.svg" alt="Logo" width={30} height={30} />
+            <h1 className="text-5xl font-bold text-pink-900">FAQs</h1>
+          </div>
+          <div>
+            {questions.map((q, index) => (
+              <div key={index}>
+                <div
+                  className="flex justify-between items-center border-b-1 pb-2 w-[100%] m-[22px_0] cursor-pointer"
+                  onClick={() => setOpenIndex(openIndex === index ? -1 : index)}
+                >
+                  <h2 className="text-[1.5rem] font-[600] text-pink-900 transition duration-300 ease hover:text-pink-800">
+                    {q.question}
+                  </h2>
+                  <span
+                    className={`bg-center bg-no-repeat min-w-[30px] min-h-[31px] ${
+                      openIndex === index
+                        ? "bg-[url(/imageMinus.svg)]"
+                        : "bg-[url(/imagePlus.svg)]"
+                    }`}
+                  ></span>
+                </div>
+                <div className={`${openIndex === index ? "block" : "hidden"}`}>
+                  <p className="text-gray-700">{q.answer}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
